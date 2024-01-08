673fc676ac39e30cc36d16365237a7c2f536ce60
KnezP <petarknezovic08@gmail.com>
2024-01-08 10:17:59 -0500

    changes made from theia lab environment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
@@ -8,9 +8,33 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
+ <AppProvider>
+    <div className='App'>
+        <h1 className='mt-3'>Shopping app</h1>
+        <div className='row mt-3'>
+
+            <div className='col-sm'>
+                <CartValue></CartValue>
+            </div>
+            <div className='col-sm'>
+                <Location></Location>
+            </div>
+            </div>
+            <h3 className='mt-3'>Shopping Cart</h3>
+            <div className='row'>
+            <div className='col-sm'>
+                <ExpenseList></ExpenseList>
+            </div>
+            </div>
+            <h3 className='mt-3'>Add Items</h3>
+            <div className='row mt-3'>
+            <div className='col-sm'>
+                <ItemSelected></ItemSelected>
+            </div>
+        </div>
     </div>
+ </AppProvider>
   );
-}
+};
 
 export default App;
